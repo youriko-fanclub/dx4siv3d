9b0961d4d107be48d45048f9fc5812c23841216b
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-10-25 20:07:55 +0900

    Arrow削除

--- input/gamepad/PlayerInput.hpp
+++ input/gamepad/PlayerInput.hpp
@@ -3,9 +3,6 @@
 #include "Button.hpp"
 #include "DPad.hpp"
 #include "Axis.hpp"
-#include "Arrow.hpp"
-
-using PlayerID = int;//du.di.Id.Player;
 
 namespace dx {
 namespace di {
@@ -15,35 +12,30 @@ namespace di {
 class IPlayerInput {
 public: // getter
     virtual const Buttons& buttons() const = 0;
-    virtual const IArrows& arrow  () const = 0;
     virtual const DPad   & dpad   () const = 0;
     virtual const IAxis  & axis   () const = 0;
+    virtual s3d::Vec2 arrowL() const = 0;
+    virtual s3d::Vec2 arrowR() const = 0;
 };
 
 /// <summary> プレイヤー入力 </summary>
 class PlayerInput : public IPlayerInput {
 public: // getter
     const Buttons& buttons() const override { return m_buttons; }
-    const IArrows& arrow  () const override { return *m_arrow ; }
     const DPad   & dpad   () const override { return m_dpad   ; }
     const IAxis  & axis   () const override { return m_axis   ; }
-    s3d::Vec2 arrowL() const { return (m_dpad.vec() + m_axis.l()).clampLength(1.f); }
-    s3d::Vec2 arrowR() const { return m_axis.r(); }
+    s3d::Vec2 arrowL() const override { return (m_dpad.vec() + m_axis.l()).clampLength(1.f); }
+    s3d::Vec2 arrowR() const override { return m_axis.r(); }
 private: // field
     const Buttons m_buttons;
-    // const ButtonsFromJoyCon m_buttons;
-    // const DPadFromKeyboard m_dpad;
     const DPad m_dpad;
     const AxisFromMultiSource m_axis;
-    std::shared_ptr<ArrowFromKeyboard> m_arrow = nullptr;
 public: // ctor/dtor
     PlayerInput() :
         m_buttons(),
         m_dpad(),
         m_axis({ std::make_shared<AxisFromKeyboard>(GamePadId::_1P),
-                 std::make_shared<AxisFromJoyCon  >(GamePadId::_1P), }) {
-        m_arrow = std::make_shared<ArrowFromKeyboard>(&m_buttons, &m_dpad, &m_axis);
-    } // TOdO:
+                 std::make_shared<AxisFromJoyCon  >(GamePadId::_1P), }) {} // TOdO:
 };
 
 }
