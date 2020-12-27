e038b37a40aab4f00154ac1368e2be0974cb8610
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-12-27 18:33:34 +0900

    音源。音を鳴らすときはまず音源を指定する。こちらも階層構造
    - 各音源は自身のAudioVolumeを持つ
    - 親音源の音量に自身の相対音量をかけ合わせた値が実際の音量になる
    - 音量変更や一斉停止処理を自身の管理下の音声に適用可能
    - 階層構造のルートにあたるAudioSourceを変更することで全音声を操作可能

---
+++ audio/AudioSource.hpp
@@ -0,0 +1,86 @@
+#pragma once
+#include <unordered_map>
+#include <Siv3D/Duration.hpp>
+#include "AssetManager.hpp"
+#include "AudioVolume.hpp"
+
+namespace dx {
+namespace aud {
+
+class AudioProxy;
+
+using AudioTag = s3d::String;
+
+class IAudioSource {
+public:
+    using Key = s3d::String;
+public:
+    virtual bool addClip(AudioType type, const Key& key, bool with_play = false) = 0;
+    virtual bool addClip(AudioType type, const Key& key, const std::vector<AudioTag>& tags, bool with_play = false) = 0;
+    virtual bool play(const Key& key, const s3d::Duration& duration = s3d::SecondsF(0.0)) = 0;
+    virtual void playOneShot(const Key& key) = 0;
+    
+    virtual void stopAll(const s3d::Duration& duration = s3d::SecondsF(0.0)) = 0;
+    virtual void stopAllJustBelow(const s3d::Duration& duration = s3d::SecondsF(0.0)) = 0;
+    virtual void stopTag(const AudioTag& tag, const s3d::Duration& duration = s3d::SecondsF(0.0)) = 0;
+    virtual AudioVolume* volume() = 0;
+
+    virtual IAudioSource* addSource(const Key& key, double relative = 1.0, bool is_mute = false) = 0;
+    virtual void removeSource(const Key& key) = 0;
+    virtual IAudioSource* source(const Key& key) = 0;
+    
+    virtual void finalize() = 0;
+    
+protected:
+    IAudioSource() = default;
+public:
+    virtual ~IAudioSource() = default;
+};
+
+
+class IAudioSourceAsVolumeChangedObserver {
+public:
+    virtual void onVolumeChanged() = 0;
+protected:
+    IAudioSourceAsVolumeChangedObserver() = default;
+public:
+    virtual ~IAudioSourceAsVolumeChangedObserver() = default;
+};
+
+
+class AudioSource final : public IAudioSource, public IAudioSourceAsVolumeChangedObserver {
+public: // static_const/enum
+public: // static
+public: // public function
+    bool addClip(AudioType type, const Key& key, bool with_play = false) override;
+    bool addClip(AudioType type, const Key& key, const std::vector<AudioTag>& tags, bool with_play = false) override;
+    bool play(const Key& key, const s3d::Duration& duration = s3d::SecondsF(0.0)) override;
+    void playOneShot(const Key& key) override;
+    
+    void stopAll(const s3d::Duration& duration = s3d::SecondsF(0.0)) override;
+    void stopAllJustBelow(const s3d::Duration& duration = s3d::SecondsF(0.0)) override;
+    void stopTag(const AudioTag& tag, const s3d::Duration& duration = s3d::SecondsF(0.0)) override;
+    virtual AudioVolume* volume() override { return &m_volume; }
+
+    IAudioSource* addSource(const Key& key, double relative = 1.0, bool is_mute = false) override;
+    void removeSource(const Key& key) override;
+    IAudioSource* source(const Key& key) override;
+    
+    void finalize() override;
+    
+    void onVolumeChanged() override;
+    
+private: // field
+    bool m_has_finalized = false;
+    std::unordered_map<s3d::String, std::shared_ptr<AudioProxy>> m_clips;
+    std::unordered_map<s3d::String, std::shared_ptr<AudioSource>> m_sources;
+    AudioVolume m_volume;
+private: // private function
+public: // ctor/dtor
+    AudioSource(const AudioVolume* const parent_volume, double relative = 1.0, bool is_mute = false);
+    ~AudioSource() override = default;
+};
+
+
+}
+}
