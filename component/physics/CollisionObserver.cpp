ca3b3a49b3bd65d3136ecf80effe4ce8b79cf393
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2021-01-19 01:38:47 +0900

    feat: 衝突イベント発生通知受信機

--- component/physics/CollisionObserver.cpp
+++ component/physics/CollisionObserver.cpp
@@ -1,8 +1,1 @@
-//
-//  CollisionObserver.cpp
-//  KANJI
-//
-//  Created by 福田和彰 on 2021/01/17.
-//
-
 #include "CollisionObserver.hpp"
--- component/physics/CollisionObserver.hpp
+++ component/physics/CollisionObserver.hpp
@@ -8,11 +8,12 @@ namespace phys {
 
 class ICollisionObserver {
 public: // static_const/enum
+    using Callback = std::function<void(std::shared_ptr<IPhysicalObject>)>;
 public: // static
 public: // public function
-    virtual void onConllisionEnter(std::weak_ptr<IPhysicalObject> that);
-    virtual void onConllisionStay(std::weak_ptr<IPhysicalObject> that);
-    virtual void onConllisionExit(std::weak_ptr<IPhysicalObject> that);
+    virtual void onCollisionEnter(const std::shared_ptr<IPhysicalObject>& that) = 0;
+    virtual void onCollisionStay(const std::shared_ptr<IPhysicalObject>& that) = 0;
+    virtual void onCollisionExit(const std::shared_ptr<IPhysicalObject>& that) = 0;
 private: // field
 private: // private function
 protected: // ctor/dtor
@@ -20,6 +21,23 @@ protected: // ctor/dtor
 public: // ctor/dtor
     virtual ~ICollisionObserver() = default;
 };
+
+
+class CollisionObserver : public ICollisionObserver {
+public: // static_const/enum
+public: // static
+public: // public function
+    void onCollisionEnter(const std::shared_ptr<IPhysicalObject>& that) override { m_callback_enter(that); }
+    void onCollisionStay(const std::shared_ptr<IPhysicalObject>& that) override { m_callback_stay(that); }
+    void onCollisionExit(const std::shared_ptr<IPhysicalObject>& that) override { m_callback_exit(that); }
+private: // field
+    Callback m_callback_enter;
+    Callback m_callback_stay;
+    Callback m_callback_exit;
+private: // private function
+public: // ctor/dtor
+};
+
 
 }
 }
